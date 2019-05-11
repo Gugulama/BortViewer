f24c2a5980f16aba0c119baa73959b50b69aa537
Gugulama <nazyrov1998@mail.ru>
2019-05-11 18:28:11 +0600

    Fix ToArray converting

--- ParserNII/DataStructures/Parser.cs
+++ ParserNII/DataStructures/Parser.cs
@@ -12,23 +12,52 @@ namespace ParserNII.DataStructures
         public DataArrays ToArray(List<DataFile> data)
         {
             var result = new DataArrays();
-
             var keys = data[0].Data.Keys;
-
-            try
+            for (int i = 1; i < data.Count; i++)
             {
-                foreach (var key in keys)
+                if (data[i].Data.Count > keys.Count)
                 {
-                    result.Data.Add(key, data.Select(d => d.Data[key]).ToArray());
+                    keys = data[i].Data.Keys;
                 }
+            }
 
-                return result;
-            }
-            catch (Exception ex)
+            foreach (var key in keys)
             {
-                Console.WriteLine(ex);
-                return result;
+                DataElement[] arr = new DataElement[data.Count];
+                for (int i = 0; i < data.Count; i++)
+                {
+                    try
+                    {
+                        arr[i] = data[i].Data[key];
+                    }
+                    catch (KeyNotFoundException e)
+                    {                        
+                        arr[i] = new DataElement
+                        {
+                            ChartValue = 0,
+                            DisplayValue = "0",
+                            Display = true
+                        };
+                    }
+                }
+                result.Data.Add(key, arr);
             }
+            return result;
+
+            //try
+            //{
+            //    foreach (var key in keys)
+            //    {
+            //        result.Data.Add(key, data.Select(d => d.Data[key]).ToArray());
+            //    }
+
+                //    return result;
+                //}
+                //catch (Exception ex)
+                //{
+                //    Console.WriteLine(ex);
+                //    return result;
+                //}
         }
 
         protected string ParseCoordinate(int coordinate)
